a47c2b280c788e6181dca6f8c62c5b254683af31
nApTu3aH82 <rinsy89@gmail.com>
2024-09-06 11:36:17 +0500

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+import fake_math as div1
+import true_math as div2
+
+result1 = div1.divide(69, 3)
+result2 = div1.divide(3, 0)
+result3 = div2.divide(49, 7)
+result4 = div2.divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
